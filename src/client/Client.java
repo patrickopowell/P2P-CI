7d8a559369391faa68d88bdbfc382c63c839c4a5
popowell <popowell@ncsu.edu>
2016-06-14 21:19:15 -0400

    Added basic client/server connection

--- src/client/Client.java
+++ src/client/Client.java
@@ -1,7 +1,23 @@
 package client;
 
+import java.io.IOException;
+import java.net.Socket;
+
 public class Client {
 
+	public Client() {
+		
+		int PortNumber = 15000;
+		
+	    Socket PClient;
+	    try {
+	           PClient = new Socket("Machine name", PortNumber);
+	    }
+	    catch (IOException e) {
+	        System.out.println(e);
+	    }
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
--- src/server/Server.java
+++ src/server/Server.java
@@ -1,10 +1,34 @@
 package server;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
+	
+	public Server() {
+		int PortNumber = 15000;
+		
+		ServerSocket PServer = null;
+	    try {
+	       PServer = new ServerSocket(PortNumber);
+	        }
+	        catch (IOException e) {
+	           System.out.println(e);
+	        }
+	    
+	    Socket clientSocket = null;
+	    try {
+	       clientSocket = PServer.accept();
+	        }
+	    catch (IOException e) {
+	       System.out.println(e);
+	    }
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		new Server();
 	}
 
 }
